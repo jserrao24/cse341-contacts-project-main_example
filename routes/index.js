c72667c1a160895e3fa770059530ba9a355c880b
Jared Serrao <jared.serrao5@gmail.com>
2024-01-13 19:23:37 -1000

    test

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {res.send('hello world');});
 
+router.get('/contacts', require('./contacts'));
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use('/' ,  require('./routes'));
+app.use('/', require('./routes'));
 
-app.listen(port, () =>(console.log(`running on port ${port}`)));
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node running on port ${port}`);
+        });
+    }
+});
